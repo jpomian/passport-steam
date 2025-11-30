40835fa72c12b4f65d9c03afed55c00c58344e6f
krrdash <jpomianowski@outlook.com>
2025-11-30 11:06:40 +0100

    Upgraded code to ES6

--- examples/signon/routes/auth.js
+++ examples/signon/routes/auth.js
@@ -1,32 +1,23 @@
-var express = require('express')
-  , router = express.Router()
-  , passport = require('passport');
 
-// GET /auth/steam
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in Steam authentication will involve redirecting
-//   the user to steamcommunity.com.  After authenticating, Steam will redirect the
-//   user back to this application at /auth/steam/return
+import express from 'express';
+import passport from 'passport';
+
+const router = express.Router();
+
 router.get('/steam',
   passport.authenticate('steam', { failureRedirect: '/' }),
-  function(req, res) {
+  (req, res) => {
     res.redirect('/');
   });
 
-// GET /auth/steam/return
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
 router.get('/steam/return',
-  // Issue #37 - Workaround for Express router module stripping the full url, causing assertion to fail 
-  function(req, res, next) {
-      req.url = req.originalUrl;
-      next();
-  }, 
+  (req, res, next) => {
+    req.url = req.originalUrl;
+    next();
+  },
   passport.authenticate('steam', { failureRedirect: '/' }),
-  function(req, res) {
+  (req, res) => {
     res.redirect('/');
   });
 
-module.exports = router;
+export default router;
---
+++ examples/signon/steamtest.js
@@ -0,0 +1,3 @@
+import steam from 'steamidconvert'
+
+steam.convertToText('76561198070656836')
